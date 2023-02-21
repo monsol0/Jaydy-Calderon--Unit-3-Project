207fbaca1cfb0829e18ff4879280770bb53bfeec
Jaydy C <126017619+monsol0@users.noreply.github.com>
2023-02-21 11:38:23 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let button1 = document.querySelector(".button-one");
+let button2 = document.querySelector(".button-two");
+let opt1 = document.querySelector(".option-one-screen");
+let opt2 = document.querySelector(".option-two-screen");
+
+button1.onclick=function(){	
+	opt1.style.display = "block"; 
+};
+
+button2.onclick=function(){
+  opt2.style.display = "block";
+};
+
+
+/* Declare variables below to save the different sections (divs) of your s
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
